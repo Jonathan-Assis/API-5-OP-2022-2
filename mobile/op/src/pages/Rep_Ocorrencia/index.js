facb104b320733cfb0f481e18a4bbf154e6a82a6
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-09-13 23:13:47 -0300

    Rotas de páginas criadas

---
+++ mobile/op/src/pages/Rep_Ocorrencia/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const Rep_Ocorrencia = () => {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.bText}>Página de Reportar Ocorrência</Text>  
+          <Text>Em desenvolvimento!</Text>
+    </View>
+  );
+}
+
+export default Rep_Ocorrencia;
--- mobile/op/src/routes/index.js
+++ mobile/op/src/routes/index.js
@@ -6,15 +6,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import User_Term from '../pages/User_Term'
 import Home from '../pages/Home'
+import Rep_Ocorrencia from '../pages/Rep_Ocorrencia'
+import Chamados from '../pages/Chamados'
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName='User_Term'>
-                <Stack.Screen name='User_Term' component={User_Term} />
-                <Stack.Screen name='Home' component={Home} />
+            <Stack.Navigator initialRouteName='Home' screenOptions={{
+                headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color ,headerStyle:{...stylesVar.toolbar}
+            }}>
+                <Stack.Screen name='User_Term' component={User_Term} options={{title:'Termos de Uso', headerShown:false}} />
+                <Stack.Screen name='Home' component={Home} options={{title:'Painel Inicial'}} />
+                <Stack.Screen name='Rep_Ocorrencia' component={Rep_Ocorrencia} options={{title:'Reportar Ocorrência'}} />
+                <Stack.Screen name='Chamados' component={Chamados} options={{title:'Chamados Abertos'}} />
             </Stack.Navigator>
         </NavigationContainer>
     );

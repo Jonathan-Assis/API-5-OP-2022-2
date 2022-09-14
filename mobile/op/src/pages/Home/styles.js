0a52fda936bc99ec0cc3b6b6b987a9b509783cdd
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-09-13 23:13:17 -0300

    Layout da tela home criado

--- mobile/op/src/pages/Home/styles.js
+++ mobile/op/src/pages/Home/styles.js
@@ -3,20 +3,53 @@ import stylesVar from '../../styles/stylesVar';
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        ...stylesVar.backgroundSecondary,
+        flex: 1
     },
+
+    header:{
+        flex:0.3,
+        margin:16,
+    },
+
+    hTitle:{
+        ...stylesVar.titlePrimary,
+        flexWrap:'wrap'
+    },
+
     body: {
         flex: 1,
-        flexDirection: 'row',
         alignItems: 'center',
         padding:10
     },
-    bText:{
-        fontSize:22,
-        fontWeight: 'bold',
+
+    bTitle:{
+        ...stylesVar.titleSecondary
+    },
+    bIcon:{
+        ...stylesVar.icon,
+    },
+    bButton:{
+        ...stylesVar.boxPrimary,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        padding:10,
+        width:130,
+        height:130,
+    },
+    bButtonTitle:{
+        ...stylesVar.titleTertiary,
+        textAlign: 'center',
+    },
+
+    bRow:{
+        flexDirection: 'row',
+    },
+    bColumn:{
+        justifyContent: 'space-around',
+        margin:20
     }
+
 })
 
 export default styles;

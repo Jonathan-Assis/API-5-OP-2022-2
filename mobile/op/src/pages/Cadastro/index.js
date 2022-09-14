c2e9c3c79c0e210217637a92451ce3a0fe572ea5
Rita2022 <rita.ferreira2529@gmail.com>
2022-09-14 01:00:10 -0300

    Criando tela Cadastro

---
+++ mobile/op/src/pages/Cadastro/index.js
@@ -0,0 +1,14 @@
+function Cadastro(){
+    return (
+        <>
+        <View style={{  backgroundColor: "#D9D9D9",flex:1}} >
+            
+        </View>
+
+
+        </>
+
+    )
+
+}
+export default Cadastro

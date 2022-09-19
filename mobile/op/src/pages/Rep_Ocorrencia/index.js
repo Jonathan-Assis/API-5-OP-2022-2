5a1951d2707504dc0ff0da7ef200e10b586ce8ac
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-09-18 23:17:37 -0300

    Adicionando estilos para o reportar ocorrência

--- mobile/op/src/pages/Rep_Ocorrencia/index.js
+++ mobile/op/src/pages/Rep_Ocorrencia/index.js
@@ -1,37 +1,89 @@
 import React, {useState} from 'react';
 import { View, Text,TextInput,TouchableOpacity, ScrollView} from 'react-native';
+import { Picker } from "@react-native-picker/picker";
+import ServerConnection from '../../services'
 import styles from './styles';
 
-import ProfileIcon from '../../assets/Icons/paperclip'
+import FileIcon from '../../assets/Icons/paperclip'
 
 const Rep_Ocorrencia = () => {
   const [cpf,setCpf]=useState('')
+  const [titulo,setTitulo]=useState('')
   const [arquivo,setArquivo]=useState({})
   const [descricao,setDescricao]=useState('')
-  
+  const [ loading, setLoading ] = useState(false);
+
+  const type = ([
+    "Poste de Luz",
+    "Árvore Caída",
+    "Pavimentação",
+    "Cano Vazando",
+  ]);
+
+  const [local,setLocal]=useState({lat:0,long:0})
+
+  const [selectedType, setSelectedType] = useState([]);
+
+
+  const newOcorrencia = () => {
+    /* if(titulo !== '' && descricao !== '') {
+        setLoading(true);
+        ServerConnection.ocorrencia({
+            cpf, titulo, categoria:selectedType,foto:arquivo, descricao, local
+        }).then(data => 
+            console.log(data.response)//mudar depois
+        ).finally(() => {
+            setLoading(false);
+        });
+    } */
+}
+
   return (
   <ScrollView>
       <View style={styles.container}>
         <View style={styles.body}> 
 
           <View style={styles.bInput}>
-            <Text style={styles.bTitle}>Identificação</Text>  
+            <Text style={styles.bTitle}>Título:</Text>  
             <TextInput style={styles.bInputStrokeBox} 
-              placeholder='000.000.000-00'
-              maxLength={8}
-              keyboardType='numeric'
+              onChangeText={setTitulo}
+              value={titulo}
+              placeholder='Título da Ocorrência'
+              placeholderTextColor={styles.bInputStrokeBox.color}
             ></TextInput>
           </View>
 
-          <View style={styles.bArquivo}>
+          <TouchableOpacity style={styles.bPickerBox}>
+                <Picker
+                  style={styles.bPickerTitle}
+                  selectedValue={selectedType}
+                  onValueChange={(itemValue, index) =>
+                    setSelectedType(itemValue, index)
+                  }
+                >
+                  {type.map((type, index) => {
+                    return (
+                      <Picker
+                        style={{ flex: 1 }}
+                        label={type}
+                        value={type}
+                        key={index}
+                      />
+                    );
+                  })}
+                </Picker>
+              </TouchableOpacity>
+
+          <TouchableOpacity style={styles.bArquivo}>
               <Text style={styles.bTitle}>Adicionar Foto</Text>
-              <Text style={styles.bTitle}>Número</Text> 
-          </View>
+              <FileIcon size={23}/> 
+          </TouchableOpacity>
 
           <View style={styles.bInput}>
             <Text style={styles.bTitle}>Ocorrido:</Text>  
             <TextInput style={styles.bInputBox} 
               placeholder='Descrição do Problema'
+              multiline={true}
             ></TextInput>  
           </View>  
                
--- mobile/op/src/pages/Rep_Ocorrencia/styles.js
+++ mobile/op/src/pages/Rep_Ocorrencia/styles.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
         flex: 1,
         margin:20,    
         padding:30,
-        ...stylesVar.boxPrimary
+        ...stylesVar.boxPrimary,
     },
 
     bImage:{
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
     },
 
     bInput:{
-        marginBottom:7,
+        marginBottom:15,
     },
 
     bTitle:{
@@ -37,18 +37,29 @@ const styles = StyleSheet.create({
         padding: 5,
         paddingHorizontal:15,
     },
+
     bInputBox:{
-        flex:1,
         textAlign: 'center',
         ...stylesVar.boxSecondary,
         ...stylesVar.placeholderSecondary,
-        padding: '30%',
+        padding: 10,
+        minHeight: 100
+    },
+    bPickerBox:{
+        ...stylesVar.strokeBoxSecondary,
+        marginBottom:10,
+
+    },
+    bPickerTitle:{
+        flexWrap: 'nowrap',        
+        ...stylesVar.titleTertiary,
     },
 
     bArquivo:{
         flexDirection:'row',
         justifyContent: 'center',
         alignItems: 'center',
+        marginVertical:10,
     },
 
     bLabel:{

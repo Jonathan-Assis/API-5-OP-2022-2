b24c2d45092470ea9d7c4bef4d571b5bdc71c0df
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-09-26 01:35:24 -0300

    Removendo opções não utilizadas

--- mobile/op/src/contexts/Auth.js
+++ mobile/op/src/contexts/Auth.js
@@ -1,9 +1,6 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { Alert } from "react-native";
-import {authService} from '../services/authService'
 import ServerConnection from '../services'
-
 
 export const AuthContext = createContext({});
 

238e6cc6a1e97843c63facf49ff65bcd9572b23b
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-09-26 01:37:49 -0300

    Ajustando Layout

--- mobile/op/src/pages/Settings/index.js
+++ mobile/op/src/pages/Settings/index.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import {useNavigation} from '@react-navigation/native'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faUserPen, faImage, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 import { useAuth } from '../../contexts/Auth';
 import styles from './styles';
 
-import ProfileIcon from '../../assets/Icons/file-person'
 
 const Settings = () => {
   const navigation = useNavigation();
-
   const { signOut } = useAuth()
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <ProfileIcon size={120} />
-        <TouchableOpacity style={styles.hButton}
-          onPress={ () => navigation.navigate('Edit_Profile')}
+        <TouchableOpacity style={styles.hContainer}
+          onPress={() => navigation.navigate('Edit_Profile')}
         >
-          <Text style={styles.hText}>Editar Perfil</Text>  
+          <View style={styles.hButton}>
+            <Text style={styles.hButtonLabel}>Editar</Text>
+            <FontAwesomeIcon icon={ faUserPen } size={26} color={'white'} />
+          </View>
         </TouchableOpacity>
       </View>
+
+      <View style={styles.body}>
+        <View style={styles.bContainer}> 
+          <View style={styles.bImage}>
+              <FontAwesomeIcon icon={ faImage } size={140} color={'white'}/>
+          </View>
+
+          <View style={styles.bRow}>
+            <Text style={styles.bTitle}>Nome</Text>  
+            <Text style={styles.bText}>Robervaldo Lopes</Text>  
+          </View>
+
+          <View style={styles.bRow}>
+            <Text style={styles.bTitle}>E-mail</Text>
+            <Text style={styles.bText}>RobervaldoFatecano@fatec.sp.gov.br</Text>
+          </View>
+        </View>
+      </View>
+
       <View style={styles.footer}>
         <TouchableOpacity style={styles.fButton}
           onPress={signOut}
         >
-            <Text style={styles.fText}>Sair</Text>  
+          <FontAwesomeIcon icon={ faRightFromBracket } size={24} color={'white'}/>
+          <Text style={styles.fLabel}>Sair</Text>
         </TouchableOpacity>
       </View>
     </View>

7986321963dd682247e4cbf9a2ac01339d4ce5e4
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-09-26 01:40:15 -0300

    Arrumando rotas

--- mobile/op/src/routes/Stack.js
+++ mobile/op/src/routes/Stack.js
@@ -1,63 +1,50 @@
 import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createStackNavigator } from '@react-navigation/stack'
 import stylesVar from '../styles/stylesVar'
 
 import * as Pages from '../pages'
 
-/*
-<StackSettings.Screen name='Cadastro' component={Pages.Cadastro} options={{title:'Cadastro'}} />
-*/
-
-const StackNavigator = () => {
-    return (
-        <Stack.Navigator initialRouteName='Start_Screen' screenOptions={{
-            headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
-        }}>
-            <Stack.Screen name='Start_Screen' component={Pages.Start_Screen} options={{title:'Start_Screen', headerShown:false}} />
-            <Stack.Screen name='User_Term' component={Pages.User_Term} options={{title:'Start_Screen', headerShown:false}} />
-        </Stack.Navigator>
-    )
-}
-
-const StackHome = createNativeStackNavigator()
+const StackHome = createStackNavigator()
 
 const StackHomeButton = () => {
     return(
         <StackHome.Navigator initialRouteName='Rep_tipo' screenOptions={{
             headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
         }}>
-            <StackHome.Screen name='Home' component={Pages.Home} options={{title:'Painel Inicial'}} />
+            <StackHome.Screen name='Home' component={Pages.Home} options={{headerShown:false}} />
             <StackHome.Screen name='Rep_Ocorrencia' component={Pages.Rep_Ocorrencia} options={{title:'Reportar Ocorrência'}} />        
             <StackHome.Screen name='Rep_Tipo' component={Pages.Rep_Tipo} options={{title:'Tipo da Ocorrência'}} />        
             <StackHome.Screen name='Maps' component={Pages.Maps} options={{headerShown:false}} />        
+            <StackHome.Screen name='Loading' component={Pages.Loading} options={{headerShown:false}} />      
+            <StackHome.Screen name='Start_Screen' component={Pages.Splash_Screen} />  
         </StackHome.Navigator>
     )
 }
 
-const StackForm = createNativeStackNavigator()
+const StackForm = createStackNavigator()
 
 const StackFormButton = () => {
     return(
         <StackForm.Navigator initialRouteName='Chamados' screenOptions={{
             headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
         }}>
-            <StackForm.Screen name='Chamados' component={Pages.Chamados} options={{title:'Chamados Abertos'}} />
+            <StackForm.Screen name='Chamados' component={Pages.Chamados} options={{headerShown:false}} />
         </StackForm.Navigator>
     )
 }
 
-const StackSettings = createNativeStackNavigator()
+const StackSettings = createStackNavigator()
 
 const StackSettingsButton = () => {
     return(
         <StackSettings.Navigator initialRouteName='Settings' screenOptions={{
             headerShown: true, headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
         }}>
-            <StackSettings.Screen name='Settings' component={Pages.Settings} options={{title:'Configurações'}} />
+            <StackSettings.Screen name='Settings' component={Pages.Settings} options={{title:'Configurações', headerShown:false}} />
             <StackSettings.Screen name='Edit_Profile' component={Pages.Edit_Profile} options={{title:'Editar Perfil'}} />
-            <StackSettings.Screen name='Log_Out' component={Pages.Sign_Out} options={{headerShown:false}} />
+            <StackSettings.Screen name='Sign_Out' component={Pages.Sign_Out} options={{headerShown:false}} />
         </StackSettings.Navigator>
     )
 }
 
-export { StackNavigator, StackHomeButton, StackFormButton, StackSettingsButton }
+export { StackHomeButton, StackFormButton, StackSettingsButton }

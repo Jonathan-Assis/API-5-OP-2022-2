c91dbbdb87976044f538f6c75f1d5fe7d9eb9c0e
Leonardo G. Ribeiro <leo.ribeiro0256@gmail.com>
2022-09-29 12:00:35 -0300

    *deletar depois*

--- mobile/op/src/pages/index.js
+++ mobile/op/src/pages/index.js
@@ -3,26 +3,21 @@ import Edit_Profile     from './Edit_Profile'
 import Home             from './Home'
 import Maps             from './Maps'
 import Sign_In          from './Sign_In'
-import Sign_Out         from './Sign_Out'
 import Sign_Up          from './Sign_Up'
 import Rep_Ocorrencia   from './Rep_Ocorrencia'
-import Rep_Tipo         from './Rep_Tipo'
+
 import Settings         from './Settings'
 import Splash_Screen     from './Splash_Screen'
 import User_Term        from './User_Term'
 
-import Loading from './Loading'
 export {
-    Loading,
     Chamados,
     Edit_Profile,
     Home,
     Maps,
     Sign_In,
-    Sign_Out,
     Sign_Up,
     Rep_Ocorrencia,
-    Rep_Tipo,
     Settings,
     Splash_Screen,
     User_Term,

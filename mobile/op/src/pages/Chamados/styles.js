3eeab7dde710b1d61cccddd9a61db0c7d1590934
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2022-10-08 12:51:07 -0300

    Removendo funções não utilizadas

--- mobile/op/src/pages/Chamados/styles.js
+++ mobile/op/src/pages/Chamados/styles.js
@@ -4,8 +4,6 @@ import stylesVar from '../../styles/stylesVar';
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     body: {
         flex: 1,
--- mobile/op/src/routes/AppRoutes.js
+++ mobile/op/src/routes/AppRoutes.js
@@ -30,7 +30,7 @@ export function AppRoutes (){
                 )}} />
                 {/* <Tab.Screen name='Center' component={StackFormButton} options={{tabBarLabel:'Chamados', tabBarIcon:({color})=>(
                 <FormIcon size={25} fill={color} /> 
-                )}} /> */}
+                )}} /> */} 
                 <Tab.Screen name='Right' component={StackSettingsButton} options={{tabBarLabel:'Opções', tabBarIcon:({color})=>(
                 <SettingsIcon size={26} fill={color} /> 
                 )}} />

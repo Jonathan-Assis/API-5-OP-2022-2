38aa6bb4b4d4bf2096b5e0598c643d796be3e823
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-02-15 00:31:23 -0300

    Adição de validação de token

--- server/src/routes/Termos.router.js
+++ server/src/routes/Termos.router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const { TermosController } = require('../controllers');
+const { handleToken } = require('../middleware');
 
-router.post('/new', TermosController.newTermo);
+router.post('/new', handleToken, TermosController.newTermo);
+router.get('/get', handleToken, TermosController.getTermo);
+router.get('/last', TermosController.getLastTermo);
 
 module.exports = router;

e8e5bd1c54268de32cc5c06841ee3169cac0061d
Sarah Santana <72801105+Sarah781@users.noreply.github.com>
2023-02-22 22:18:30 -0300

    Update sendEmail.js

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,4 +1,8 @@
 const nodemailer = require('nodemailer');
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient('mongodb+srv://root:root@cluster0.a0mnjqy.mongodb.net/opdb')
+const opdb = client.db('opdb');
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -8,21 +12,26 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-const mailOptions = {
-  from: 'nox20231@gmail.com',
-  to: 'sarah7bvs@gmail.com',
-  subject: 'Alerta de vazamento de dados',
-  html: '<body> <div style="background-color: #ffcccc; border: 1px solid #ff0000; padding: 15px; border-radius: 25px; width: 500px; " >' +
-    '<p><strong>ALERTA DE VAZAMENTO DE DADOS:</strong></p>' +
-    '<p>Caro usuário, infelizmente detectamos um vazamento de dados em nossos sistemas. Um terceiro não autorizado acessou seu nome, CPF, endereço de email e senha.</p>' +
-    '<p>Estamos agindo rapidamente para proteger sua conta. Por favor, altere suas senhas imediatamente!</p>' +
-    '<div><center><strong>Em caso de dúvidas, fique a vontade de entrar em contato!</center></strong></div></div></body>'
-};
+async function main() {
+  const emails = (await opdb.collection('cidadao').find().toArray()).map(cidadao => cidadao.email);
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+  const mailOptions = {
+    from: 'nox20231@gmail.com',
+    to: emails.join(','),
+    subject: 'Alerta de vazamento de dados',
+    html: '<body> <div style="background-color: #ffcccc; border: 1px solid #ff0000; padding: 15px; border-radius: 25px; width: 500px; " >' +
+      '<p><strong>ALERTA DE VAZAMENTO DE DADOS:</strong></p>' +
+      '<p>Caro usuário, infelizmente detectamos um vazamento de dados em nossos sistemas. Um terceiro não autorizado acessou seu nome, CPF, endereço de email e senha.</p>' +
+      '<p>Estamos agindo rapidamente para proteger sua conta. Por favor, altere suas senhas imediatamente!</p>' +
+      '<div><center><strong>Em caso de dúvidas, fique a vontade de entrar em contato!</center></strong></div></div></body>'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+main().finally(() => client.close());

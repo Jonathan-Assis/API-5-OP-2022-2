00456f58a97c9b6e5495cf2f8dc59e5d2698d76e
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-06-12 00:10:02 -0300

    melhoria no loading

--- mobile/src/components/Loading/index.js
+++ mobile/src/components/Loading/index.js
@@ -1,70 +1,34 @@
-import React, { useEffect } from 'react';
-import { View, Text, Animated } from 'react-native';
+import React from 'react'
+import { Text } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import LogoOP from '../../assets/Logotype/LogoOP.svg'
+import Animated, { BounceIn, BounceOut } from 'react-native-reanimated'
 
 import styles from './styles'
 
 export default function Loading({ loading, children, title }) {
-    const animations = {
-        one: new Animated.Value(0),
-        two: new Animated.Value(0),
-        three: new Animated.Value(0),
-    }
-
-    function onAnimate(animation, nextAnimation){
-        Animated.sequence([
-            Animated.timing(animation, {
-                toValue:-15,
-                duration:390,
-                useNativeDriver: true,
-            }),
-            Animated.timing(animation, {
-                toValue:15,
-                duration:390,
-                useNativeDriver: true,
-            })
-        ]).start()
-        setTimeout(nextAnimation, 300)
-    }
-
-    function onStartAnimate(){
-        function onThreeAnimation(){
-            onAnimate(animations.three, () =>{
-                setTimeout(onStartAnimate, 300)
-            })
-        }
-        function onTwoAnimation(){
-            onAnimate(animations.two,onThreeAnimation)
-        }
-        onAnimate(animations.one,onTwoAnimation)
-    }
-
-    useEffect(()=>{
-       onStartAnimate()
-    })
-
     return (
         loading
             ? (
-                <View style={styles.container}>
+                <LinearGradient
+                colors={['#4444EE', '#4944EE', '#4944EE', '#4444EE']}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 1 }}
+                style={styles.container}
+              >
+                <Animated.View
+                    entering={BounceIn}
+                >
+                    <LogoOP style={styles.logo} />
+                </Animated.View>
+                <Text style={styles.title}>
                     {
-                        title && 
-                            <Text style={styles.title}>{title}</Text>
+                        title ? title : 'Carregando...'
                     }
-                        <View style={styles.body}>
-                        <Animated.View style={[
-                            styles.bBall, 
-                            {transform:[{translateY: animations.one}]}]} />
-                        <Animated.View style={[
-                            styles.bBall,
-                            {transform:[{translateY: animations.two}]}
-                            ]} />
-                        <Animated.View style={[
-                            styles.bBall,
-                            {transform:[{translateY: animations.three}]}
-                            ]} />
-                    </View>
-                </View>
-            )
+                </Text>
+               </LinearGradient>
+                )
             : <>{ children }</>
+           
     );
 }

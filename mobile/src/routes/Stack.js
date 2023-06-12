1704fe5d6c2f79931ad26eebbaf683d21326b15c
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-06-12 00:08:59 -0300

    Ajustes de cores e imports

--- mobile/src/routes/Stack.js
+++ mobile/src/routes/Stack.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import stylesVar from '../styles/stylesVar'
+import { Colors, StyledColors } from '../styles'
 
 import * as Pages from '../pages'
 
@@ -9,11 +9,33 @@ const StackHome = createStackNavigator()
 const StackHomeButton = () => {
     return(
         <StackHome.Navigator initialRouteName='Home' screenOptions={{
-            headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
+            headerTitleAlign: 'center',
+            headerTintColor: Colors.white,
+            headerStyle:{
+                ...StyledColors.background.Primary
+            }
         }}>
-            <StackHome.Screen name='Home' component={Pages.Home} options={{headerShown:false}} />
-            <StackHome.Screen name='Rep_Ocorrencia' component={Pages.Rep_Ocorrencia} options={{title:'Reportar Ocorrência'}} />        
-            <StackHome.Screen name='Maps' component={Pages.Maps} options={{headerShown:false}} />        
+            <StackHome.Screen
+                name='Home'
+                component={Pages.Home}
+                options={{
+                    headerShown:false
+                }}
+            />
+            <StackHome.Screen
+                name='Rep_Ocorrencia'
+                component={Pages.Rep_Ocorrencia}
+                options={{
+                    title:'Reportar Ocorrência'
+                    }}
+            />        
+            <StackHome.Screen
+                name='Maps'
+                component={Pages.Maps}
+                options={{
+                    headerShown:false
+                }}
+            />        
         </StackHome.Navigator>
     )
 }
@@ -23,9 +45,19 @@ const StackForm = createStackNavigator()
 const StackFormButton = () => {
     return(
         <StackForm.Navigator initialRouteName='Chamados' screenOptions={{
-            headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
+            headerTitleAlign: 'center',
+            headerTintColor: Colors.white,
+            headerStyle:{
+                ...StyledColors.background.Primary
+            }
         }}>
-            <StackForm.Screen name='Chamados' component={Pages.Chamados} options={{headerShown:false}} />
+            <StackForm.Screen
+                name='Chamados'
+                component={Pages.Chamados}
+                options={{
+                    headerShown:false
+                }}
+            />
         </StackForm.Navigator>
     )
 }
@@ -35,10 +67,28 @@ const StackSettings = createStackNavigator()
 const StackSettingsButton = () => {
     return(
         <StackSettings.Navigator initialRouteName='Settings' screenOptions={{
-            headerShown: true, headerTitleAlign: 'center', headerTintColor: stylesVar.toolbarTitle.color, headerStyle:{...stylesVar.toolbar}
+            headerShown: true,
+            headerTitleAlign: 'center',
+            headerTintColor: Colors.white,
+            headerStyle:{
+                ...StyledColors.background.Primary
+            }
         }}>
-            <StackSettings.Screen name='Settings' component={Pages.Settings} options={{title:'Configurações', headerShown:false}} />
-            <StackSettings.Screen name='Edit_Profile' component={Pages.Edit_Profile} options={{title:'Editar Perfil'}} />
+            <StackSettings.Screen
+                name='Settings'
+                component={Pages.Settings}
+                options={{
+                    title:'Configurações',
+                    headerShown:false
+                }}
+            />
+            <StackSettings.Screen
+                name='Edit_Profile'
+                component={Pages.Edit_Profile}
+                options={{
+                    title:'Editar Perfil'
+                }}
+            />
         </StackSettings.Navigator>
     )
 }
